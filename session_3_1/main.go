cee5cd0d6cf3bff6fcd6b90293d088d8fc8ed1cc
AmirHossein Shirazi <ashzih2@gmail.com>
2026-05-06 16:24:51 +0330

    pointers

---
+++ session_3_1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// pointer's zero value is nil
+	var p *int
+	i := 43
+
+	fmt.Println("p", p)
+	fmt.Println("i", i)
+
+	p = &i
+
+	fmt.Println("p after assigning i's pointer to it:", p)
+	fmt.Println("value that p is refrencing to:", *p, "\n")
+
+	d := 44
+	p = &d
+	fmt.Printf("&i: %p, &d: %p\n", &i, &d)
+	fmt.Printf("&d: %p, p: %p, *p: %d\n", &d, p, *p)
+	*p = d
+	fmt.Printf("&d: %p, p: %p, *p: %d\n", &d, p, *p)
+}
